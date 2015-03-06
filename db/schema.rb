a931f6004879636cc42e5b1beff6df9236855611
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2015-03-06 12:57:17 -0700

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301035243) do
+ActiveRecord::Schema.define(version: 20150306193851) do
+
+  create_table "bands", force: :cascade do |t|
+    t.string   "band_name"
+    t.string   "members"
+    t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "event_name"
